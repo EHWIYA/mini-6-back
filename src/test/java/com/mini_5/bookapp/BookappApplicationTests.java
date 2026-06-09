2eb241845ad507449c5a4f4dc1dc857acead5dd7
hywznn <choihj0510@naver.com>
2026-06-09 11:07:14 +0900

    initial

---
+++ src/test/java/com/mini_5/bookapp/BookappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini_5.bookapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
