21b9dc1de5d48ec902adc79ffacfb420c3348064
minwook-Kang <72792893+minwook-Kang@users.noreply.github.com>
2026-06-09 15:09:07 +0900

    Update BookRepository.java

--- src/main/java/com/aivle/bookapp/repository/BookRepository.java
+++ src/main/java/com/aivle/bookapp/repository/BookRepository.java
@@ -1,4 +1,9 @@
 package com.aivle.bookapp.repository;
 
-public class BookRepository {
+import com.aivle.bookapp.domain.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
+
 }
